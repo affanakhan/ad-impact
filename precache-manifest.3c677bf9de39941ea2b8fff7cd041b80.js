a5b123122eca5ba4650f0e01099b9894252f1fa4
Anisha <bluestar225@gmail.com>
2020-11-24 20:12:35 -0800

    Updates

---
+++ precache-manifest.3c677bf9de39941ea2b8fff7cd041b80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc252f6a41b152dac5d5ca1d34c070ca",
+    "url": "/affanakhan/index.html"
+  },
+  {
+    "revision": "817e342f8d2f65c161ba",
+    "url": "/affanakhan/static/css/2.60653a47.chunk.css"
+  },
+  {
+    "revision": "b302bb64e4c8fdd44855",
+    "url": "/affanakhan/static/css/main.3be71844.chunk.css"
+  },
+  {
+    "revision": "817e342f8d2f65c161ba",
+    "url": "/affanakhan/static/js/2.9461006a.chunk.js"
+  },
+  {
+    "revision": "a893df35f1576061d450930806356b15",
+    "url": "/affanakhan/static/js/2.9461006a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b302bb64e4c8fdd44855",
+    "url": "/affanakhan/static/js/main.0b03c264.chunk.js"
+  },
+  {
+    "revision": "bdf84fd31244d13579b1",
+    "url": "/affanakhan/static/js/runtime-main.c559dc85.js"
+  }
+]);
